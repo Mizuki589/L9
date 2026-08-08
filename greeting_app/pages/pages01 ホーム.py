6249155fdc45bbe28cb5a3ec7f19a078a151ee11
Mizuki <mizuki.k2013@gmail.com]>
2026-08-08 12:06:41 +0900

    名前の変更

---
+++ greeting_app/pages/pages01 ホーム.py
@@ -0,0 +1,21 @@
+import streamlit as st
+st.title("表示")
+
+if ("user_name"in st.session_state and st.session_state.user_name):
+    st.success("保存されている情報")
+    col1,col2=st.columns(2)
+    with col1:
+        st.metric("名前", st.session_state.user_name)
+        st.metric("学年", st.session_state.get('user_gakunen', '未設定'))
+    
+    with col2:
+        if st.session_state.get("user_hobby"):
+            st.write("趣味")
+            for hobby in st.session_state.user_hobby:
+                st.write(f"{hobby}")
+        else:
+            st.write("趣味：未設定")
+
+else:
+    st.error("ユーザー名が設定されていません")
+    st.write("メインページで名前を入力してください")
