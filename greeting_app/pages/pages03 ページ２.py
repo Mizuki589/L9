2a858a7dfd3ac8bdd7a869abc8ad732c2b5d7fa6
Mizuki <mizuki.k2013@gmail.com]>
2026-09-12 12:45:24 +0900

    レシピ検索機能を追加し、食材入力と検索ボタンを実装

--- greeting_app/pages/pages03 ページ２.py
+++ greeting_app/pages/pages03 ページ２.py
@@ -1,2 +1,19 @@
+import requests
 import streamlit as st
-st.write("おはようorこんにちはorこんばんは")
+import random
+st.title("レシピ検索")
+food = st.text_input("英語で食材を入力してください（例: chicken）")
+num=random.randint(0, 20)
+if st.button("検索"):
+    if food:
+        l = "https://www.themealdb.com/api/json/v1/1/filter.php"
+        response = requests.get(l, params={"i": food})
+        data = response.json()
+        if data["meals"]:
+            meal = data["meals"][num]#左の[０]を変えることができる。
+            st.subheader(meal["strMeal"])
+            st.image(meal["strMealThumb"])
+        else:
+            st.write("レシピが見つかりませんでした。")
+    else:
+        st.write("食材を入力してください。")
